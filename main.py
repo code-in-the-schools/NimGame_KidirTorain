48c64f49a0ed33c4d0420f86ada5c03bd19acd39
Kidir Torain <kktorainnn@gmail.com>
2020-07-27 17:33:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#player1 
+#player2  
+#player 1 goes first 
+
+def my_function():
+
+
+running = True 
+while running:
+
+#if player1 is true print "player 1" if else print "player 2"
+#print score
+#function 2
+
+
+
+print("P1 wins!!!!!")
+if else:
+  print("P2 wins!!!")
+
+
+
