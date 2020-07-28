39e80ff9d36f0b53e275895d36fe7fabfc587cd4
Kidir Torain <kktorainnn@gmail.com>
2020-07-28 15:43:00 +0000

    coding

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #player1 
 #player2  
-#player 1 goes first 
-
+score = 0 
+import random #randomly choose 1 or 2 on its turn 
 def my_function():
-
-
-running = True 
+print("")
+  running = True 
 while running:
-
 #if player1 is true print "player 1" if else print "player 2"
 #print score
 #function 2
 
+  if player1 == True: 
+    print("P1 its your turn")
+if else:
+  print("P2 its your turn")
 
-
+print("P1, enter a number...")
+print("P2, enter a number...")
+score = 
 print("P1 wins!!!!!")
 if else:
   print("P2 wins!!!")
